87cdf423d513f06768eba19c9213fbd33b9f4345
boiv0001 <benjamin.boivin@etudiant.univ-reims.fr>
2019-06-14 10:54:08 +0200

    Ajout documentation classe Pastille

--- page/class/Pastille.js
+++ page/class/Pastille.js
@@ -1,14 +1,73 @@
 class Pastille {
 
+    /**
+     * Constructeur
+     * @param {*} sens Sens de la pastille (haut, bas, gauche, droite, centre)
+     * @param {*} vitesse Vitesse de la pastille
+     * @param {*} touche Touche concerner par la pastille
+     */
     constructor(sens, vitesse, touche) {
+
+        /**
+         * @var string Sens de la pastille
+         */
         this.sens = sens;
+
+        /**
+         * @var int Vitesse de la pastille
+         */
         this.vitesse = vitesse;
+
+        /**
+         * @var Touche Touche concerner par la pastille
+         */
         this.touche = touche;
+
+        /**
+         * @var int Taille du trajet de la pastille en pixel
+         */
         this.taille = 200;
+
+        /**
+         * @var string Couleur de la pastille
+         */
         this.couleur = "#00FF00";
+
+        /**
+         * @var int Position x de départ de la pastille
+         */
         this.baseX = 0;
+
+        /**
+         * @var int Position y de départ de la pastille
+         */
         this.baseY = 0;
+
+        /**
+         * @var int Coin de la pastille
+         */
         this.corner = 0;
+
+        /**
+         * @var int Position actuelle en x de la pastille
+         */
+        this.x = 0;
+
+        /**
+         * @var int Position actuelle en y de la pastille 
+         */
+        this.y = 0;
+
+        /**
+         * @var int Largeur actuelle de la pastille
+         */
+        this.width = 0;
+
+        /**
+         * @var int Hauteur actuelle de la pastille
+         */
+        this.height = 0;
+
         if(this.sens == "haut"){
             this.width = 30;
             this.height = 10;
@@ -40,6 +99,9 @@ class Pastille {
         this.y = this.baseY;
     }
 
+    /**
+     * Deplace la pastille d'un cran par rapport a la vitesse
+     */
     move(){
         var finished = false;
         if(this.sens == "haut"){
@@ -74,6 +136,9 @@ class Pastille {
         return !finished;
     }
 
+    /**
+     * Affiche la pastille
+     */
     draw(){
         if(this.move()){
             stroke("#FF0000");
@@ -82,6 +147,9 @@ class Pastille {
         }
     }
 
+    /**
+     * Donne le temps en seconde du trajet de la pastille
+     */
     getTime(){
         var time = 0;
         time = this.taille / this.vitesse;
@@ -89,6 +157,9 @@ class Pastille {
         return time;
     }
 
+    /**
+     * Donnne le milieu de la touche en hauteur ou en largeur en fonction du sens de la pastille
+     */
     getMid(){
         var res = this.touche.button.width;
         if(this.width >= this.height){
@@ -99,6 +170,9 @@ class Pastille {
         return Math.trunc(res/2);
     }
 
+    /**
+     * Donne la vitesse de grossissement de la pastille central par rapport a la vitesse
+     */
     getGrowingSpeed(){
         var grow = this.touche.width / 2;
         grow /= this.taille / this.vitesse;
